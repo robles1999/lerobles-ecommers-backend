a0f0d0e4c672b84ede2fdfb6996dbe8caa32dc09
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-05-02 18:44:24 -0400

    minor corrections on models.

--- models/Category.js
+++ models/Category.js
@@ -1,37 +1,15 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
-    product_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      defaultValue: 10,
-      allowNull: false,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Category,
-        key: "id",
-      },
     },
   },
   {
