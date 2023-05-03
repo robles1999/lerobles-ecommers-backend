3c4f913012e089d6b336e93817664dd84789e65a
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-05-03 09:44:47 -0400

    added table joins.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require("./config/connection");
 
 const app = express();
@@ -12,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+// The force option determines whether or not to drop and recreate
+// the tables if they already exist.In this case, force: false means
+// that the tables will not be dropped and recreated.
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
